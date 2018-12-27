c712a4b0358b960915bdae6e58adadce28245809
FromtRowWithJ <adebusum@tcd.ie>
2018-12-27 18:48:56 +0000

    Added changes to the method genQRImage: Data modules are colored red if they the data value for that module is unknown.

--- QRImage.java
+++ QRImage.java
@@ -6,6 +6,10 @@ import javax.imageio.ImageIO;
 import java.util.Random;
 
 public class QRImage {
+    private final static int BLACK = 0x00000000;
+    private final static int WHITE = 0xFFFFFFFF;
+    private final static int RED   = 0x00FF0000;
+
     public static void main(String... args) {
         Random r = new Random();
         int[][] qrdata = new int[50][50];
@@ -18,11 +22,12 @@ public class QRImage {
     public static void genQRImage(int[][] QRData, int moduleSize, String directory) {
         try {
             int side = QRData.length;
-            BufferedImage bi = new BufferedImage(side * moduleSize, side * moduleSize, BufferedImage.TYPE_BYTE_BINARY);
+            BufferedImage bi = new BufferedImage(side * moduleSize, side * moduleSize, BufferedImage.TYPE_INT_RGB);
             for (int i = 0; i < QRData.length; i++)
                 for (int j = 0; j < QRData.length; j++) {
                     int[] cArray = new int[moduleSize * moduleSize];
-                    Arrays.fill(cArray, QRData[i][j] % 2 == 1 ? 0x00000000 : 0xFFFFFFFF);
+                    Arrays.fill(cArray, QRData[i][j] == -1 ? RED : QRData[i][j] % 2 == 1 ? BLACK : WHITE);
+                    // Arrays.fill(cArray, QRData[i][j] % 2 == 1 ? 0x00000000 : 0xFFFFFFFF);
                     bi.setRGB(j * moduleSize, i * moduleSize, moduleSize, moduleSize, cArray, 0, 0);
                 }
             ImageIO.write(bi, "PNG", new File(directory + "QRImage.png"));
