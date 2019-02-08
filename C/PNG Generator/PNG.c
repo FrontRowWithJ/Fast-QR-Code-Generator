da01dfb9e7e7a610703e489d4e2e9df6c7b73d46
FrontRowWithJ <adebusum@tcd.ie>
2019-02-08 09:09:11 +0000

    It works but with a small caveat, only for png files for less than 32kb because it doesn't correctly handle IDAT's handle IDAT's Will be using the libpng in the future

--- C/PNG Generator/PNG.c
+++ C/PNG Generator/PNG.c
@@ -1,11 +1,5 @@
 #include "PNG.h"
 
-int main(int argc, char **argv)
-{
-    QRCode qr = init(0, "HELLO WORLD", false);
-    export_to_png(qr, 1, "qrImage.png");
-}
-
 int add_png_signature(FILE *png_file)
 {
     if (png_file == NULL)
@@ -114,8 +108,8 @@ PLTE gen_grayscale_plte()
 {
     PLTE plte;
     plte.entries = (Entry *)malloc(sizeof(Entry) * 2);
-    plte.entries[1] = (Entry){255, 255, 255};
-    plte.entries[0] = (Entry){0, 0, 0};
+    plte.entries[0] = (Entry){255, 255, 255};
+    plte.entries[1] = (Entry){0, 0, 0};
     plte.numOfEntries = 2;
     return plte;
 }
@@ -128,7 +122,7 @@ int add_plte_chunk(FILE *png_file, PLTE *plte)
     byte *buf = (byte *)calloc(BUFFER_LENGTH, sizeof(byte));
     int plteDataLen = plte->numOfEntries * sizeof(Entry);
     for (int i = sizeof(int) - 1; i > -1; i--)
-        fputc((plteDataLen >> (i * 8)) & 0xFF, png_file);
+        fputc((plteDataLen >> (i * 8)), png_file);
     for (int i = 0; i < CHUNK_TYPE_LENGTH; i++)
         buf[bufLen++] = PLTE_TYPE[i];
     for (int i = 0; i < plte->numOfEntries; i++)
@@ -148,107 +142,90 @@ int add_plte_chunk(FILE *png_file, PLTE *plte)
     return SUCCESS;
 }
 
-int qr_data_to_idat_bit_stream(byte_t **QRData, size_t QRWidth, BitSet *bs, size_t *byteLen)
+byte *qr_data_to_idat_bit_stream(size_t QRWidth, byte QRData[QRWidth][QRWidth], size_t *bufLen)
 {
-    int index = 0;
-    bs->len = QRWidth * QRWidth + 88 - ((QRWidth * QRWidth) % 8); // default compression bytes  16
-                                                                  // nlen                       16
-                                                                  // len                        16
-                                                                  // adler-32 checksum          32
-                                                                  // header bits               + 8
-                                                                  //                            __
-                                                                  //                            88 bits
-
-    *byteLen = (bs->len / 8 + (bs->len % 8 == 0 ? 0 : 1));
-    bs->bits = (byte_t *)calloc(*byteLen, sizeof(byte_t));
-    bs->bits[0] = (byte)0x78;
-    bs->bits[1] = (byte)0x9C;
-    bs->bits[2] = (byte)0x01;
-    int len = QRWidth * QRWidth / 8;
+    *bufLen = QRWidth * QRWidth + 11;
+    byte *buf = (byte *)malloc(*bufLen * sizeof(byte));
+    // default compression bytes  16
+    // nlen                       16
+    // len                        16
+    // adler-32 checksum          32
+    // header bits               + 8
+    //                            __
+    //                            88 bits == 11 bytes
+    buf[0] = (byte)0x78;
+    buf[1] = (byte)0x9C;
+    buf[2] = (byte)0x01;
+    int len = QRWidth * QRWidth;
     int nlen = ~len;
-    bs->bits[3] = (byte)(len >> 8);
-    bs->bits[4] = (byte)(len & 0xFF);
-    bs->bits[5] = (byte)(nlen >> 8);
-    bs->bits[6] = (byte)(nlen & 0xFF);
-    index = 56;
-    // for (int i = 0; i < sizeof(short) * 8; i++)
-    //     if (set_bit(bs, index++, (len >> i) & 1))
-    //         return ERROR;
-    // for (int i = 0; i < sizeof(short) * 8; i++)
-    //     if (set_bit(bs, index++, (nlen >> i) & 1))
-    //         return ERROR;
-    for (int i = 0; i < len * 8; i++)
-        if (set_bit(bs, index++, QRData[i / QRWidth][i % QRWidth]))
-            return ERROR;
-    BitSet qrBs;
-    int result = qr_data_to_bit_stream(QRData, QRWidth, &qrBs);
-    if (result != SUCCESS)
-        return result;
-    unsigned long adler = update_adler32(qrBs.bits, qrBs.len / 8 + (qrBs.len % 8 == 0 ? 0 : 1));
-    for (int i = 0; i < sizeof(int) * 8; i++)
-        if (set_bit(bs, index++, (adler >> i) & 1))
-            return ERROR;
-    return SUCCESS;
+    buf[3] = (byte)((len >> 8) & 0xFF);
+    buf[4] = (byte)(len & 0xFF);
+    buf[5] = (byte)((nlen >> 8) & 0xFF);
+    buf[6] = (byte)(nlen & 0xFF);
+    int index = 7;
+    for (int i = 0; i < len; i++)
+        buf[index++] = QRData[i / QRWidth][i % QRWidth] % 2;
+    size_t qrBufLen;
+    byte *qrBuf = qr_data_to_bit_stream(QRWidth, QRData, &qrBufLen);
+    if (qrBuf == NULL)
+        return NULL;
+    unsigned long adler = update_adler32(qrBuf, qrBufLen - 4);
+    for (int i = 3; i > -1; i--)
+        buf[index++] = (adler >> i * 8) & 0xFF;
+    free(qrBuf);
+    return buf;
 }
 
-int add_idat_chunk(FILE *png_file, byte **magqrData, size_t magLen)
+int add_idat_chunk(FILE *png_file, size_t magLen, byte magQR[magLen][magLen])
 {
     if (png_file == NULL)
         return ERROR;
     int byteCount = 0;
-    BitSet bs;
+    byte *buf;
     size_t byteLen;
-    if (qr_data_to_idat_bit_stream(magqrData, magLen, &bs, &byteLen))
+    buf = qr_data_to_idat_bit_stream(magLen, magQR, &byteLen);
+    if (buf == NULL)
         return ERROR;
     for (int i = sizeof(int) - 1; i > -1; i--)
         fputc((byteLen >> 8 * i) & 0xFF, png_file);
     for (int i = 0; i < CHUNK_TYPE_LENGTH; i++)
         fputc(IDAT_TYPE[i], png_file);
     for (int i = 0; i < byteLen; i++)
-        fputc(bs.bits[i], png_file);
+        fputc(buf[i], png_file);
     byte crcBuf[CHUNK_TYPE_LENGTH + byteLen];
     for (int i = 0; i < CHUNK_TYPE_LENGTH; i++)
         crcBuf[i] = IDAT_TYPE[i];
     for (int i = 0; i < byteLen; i++)
-        crcBuf[i + CHUNK_TYPE_LENGTH] = bs.bits[i];
+        crcBuf[i + CHUNK_TYPE_LENGTH] = buf[i];
     unsigned long crc = update_crc(crcBuf, CHUNK_TYPE_LENGTH + byteLen);
     for (int i = sizeof(int) - 1; i > -1; i--)
         fputc((crc >> i * 8) & 0xFF, png_file);
+    free(buf);
     return SUCCESS;
 }
 
-byte_t **magnify_data(QRCode qr, int factor, size_t *newLen)
+void magnify_data(QRCode qr, size_t magWidth, int factor, byte magQRData[magWidth][magWidth])
 {
     if (factor <= 0)
-        return NULL;
+        return;
     if (factor > 255)
-        return NULL;
-    *newLen = qr.QRWidth * factor;
-    byte_t **magnifiedData = (byte **)malloc(*newLen * sizeof(byte));
-    for (int i = 0; i < *newLen; i++)
-        magnifiedData[i] = (byte *)malloc(*newLen * sizeof(byte));
+        return;
     for (int i = 0; i < qr.QRWidth; i++)
         for (int j = 0; j < qr.QRWidth; j++)
             for (int fi = 0; fi < factor; fi++)
                 for (int fj = 0; fj < factor; fj++)
-                    magnifiedData[i * factor + fi][j * factor + fj] = qr.QRData[i][j] % 2;
-    return magnifiedData;
+                    magQRData[i * factor + fi][j * factor + fj] = qr.QRData[i][j] % 2;
 }
 
-int qr_data_to_bit_stream(byte_t **QRData, size_t QRWidth, BitSet *bs)
+byte *qr_data_to_bit_stream(size_t QRWidth, byte QRData[QRWidth][QRWidth], size_t *bufLen)
 {
-    bs->len = QRWidth * QRWidth - ((QRWidth * QRWidth) % 8);
-    bs->bits = (byte_t *)calloc(bs->len / 8 + (bs->len % 8 == 0 ? 0 : 1), sizeof(byte_t));
     int i = 0;
     byte_t codeword = 0;
-    for (; i < bs->len; i += 8)
-    {
-        for (int j = 0; j < 8 && (i + j) < bs->len; j++)
-            codeword += (QRData[(i + j) / QRWidth][(i + j) % QRWidth] & 1) << j;
-        bs->bits[i / 8] = codeword;
-        codeword = 0;
-    }
-    return SUCCESS;
+    *bufLen = QRWidth * QRWidth;
+    byte *buf = (byte *)malloc(*bufLen * sizeof(byte));
+    for (int i = 0; i < *bufLen; i++)
+        buf[i] = QRData[i / QRWidth][i % QRWidth];
+    return buf;
 }
 
 unsigned long update_adler32(byte_t *buf, int len)
@@ -293,17 +270,18 @@ int export_to_png(QRCode qr, int factor, const char *filename)
         }
         free(str);
     }
-    size_t magLen;
-    byte_t **magQR = magnify_data(qr, factor, &magLen);
+    size_t magLen = factor * qr.QRWidth;
+    byte magQR[magLen][magLen];
+    magnify_data(qr, magLen, factor, magQR);
     FILE *qr_img = fopen(png_name, "wb");
     add_png_signature(qr_img);
-    IHDR ihdr = gen_IHDR(magLen, magLen, 1, 3, 0, 0, 0);
+    IHDR ihdr = gen_IHDR(magLen - 1, magLen - 1, 8, 3, 0, 0, 0);
     add_ihdr_chunk(qr_img, ihdr);
     PLTE grayscale = gen_grayscale_plte();
     add_plte_chunk(qr_img, &grayscale);
-    add_idat_chunk(qr_img, magQR, magLen);
+    add_idat_chunk(qr_img, magLen, magQR);
     add_iend_chunk(qr_img);
-    // free_byte_matrix(magQR);
+    // free_byte_matrix(magQR, magLen);
     fclose(qr_img);
     qr_img = NULL;
     free(png_name);
