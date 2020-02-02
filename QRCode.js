10bf75edb55acd5ade42a27dbd2effa389645436
Michalade <adebusum@tcd.ie>
2020-02-02 22:44:33 +0000

    Update QRCode.js
    Made small changes

--- QRCode.js
+++ QRCode.js
@@ -1,19 +1,19 @@
-let func = (function QRCode() {
+let QRCode = (()=>{
     let canRun = false;
     Module.onRuntimeInitialized = () => canRun = true;
     const BUFFER_LENGTH = 32768;
     let QRData;
     let output_ptr;
     let gen_qr_code;
-    let has_init = true;
+    let has_init = false;
     let gen_qr = function (message, ECL) {
         if (canRun) {
-            if (has_init) {
+            if (!has_init) {
                 QRData = new Int8Array(BUFFER_LENGTH);
                 gen_qr_code = Module.cwrap("gen_qr", null, ["number", "string", "number"]);
                 output_ptr = Module._malloc(QRData.length);
                 Module.HEAP8.set(QRData, output_ptr);
-                has_init = false;
+                has_init = true;
             }
             gen_qr_code(ECL, message, output_ptr);
             const result = {size: undefined, QRData: undefined};
@@ -26,7 +26,7 @@ let func = (function QRCode() {
                     row.push(+Module.getValue(output_ptr + 1 + i * size + j, "i8") & 1);
                 result.QRData.push(row);
             }
-            // Module._free(output_ptr);
+            Module._free(output_ptr);
             QRData = null;
             return result;
         }
@@ -34,7 +34,3 @@ let func = (function QRCode() {
     }
     return gen_qr;
 })();
-
-setInterval(() => {
-    let a = func("Hello World", 0);
-}, 1000);
